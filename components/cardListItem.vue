7c4ea2559c5b86d0dfa01c323ae5ba4e60d66933
fdemoncade <f.demoncade@gmail.com>
2023-08-23 14:09:13 +0200

    WIP : Card List

---
+++ components/cardListItem.vue
@@ -0,0 +1,8 @@
+<template>
+  <h4>{{ blok.title }}</h4>
+  <h5>WIP</h5>
+</template>
+
+<script setup>
+const props = defineProps({ blok: Object });
+</script>
---
+++ storyblok/cardList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div
+    v-editable="blok"
+    class="container mx-auto grid md:grid-cols-3 gap-12 my-12 place-items-center"
+  >
+  <h2>{{ blok.title }}</h2>
+    <cardListItem
+      v-for="blok in blok.items"
+      :key="blok._uid"
+      :blok="blok"
+    />
+  </div>
+</template>
+ 
+<script setup>
+defineProps({ blok: Object })
+</script>
