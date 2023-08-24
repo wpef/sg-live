41d7553033182a3afa51e3291d11dd69446df091
fdemoncade <f.demoncade@gmail.com>
2023-08-24 14:47:09 +0200

    DONE : cardGrid

---
+++ components/cardGridItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="cardGridItem">
+    <div class="content">
+      <h4 v-if="blok.title" class="h5 pb-4">{{ blok.title }}</h4>
+      <p v-if="blok.description" class="p2">{{ blok.description }}</p>
+    </div>
+    <CTA v-if="blok.cta[0]" :blok="blok.cta[0]" />
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({ blok: Object });
+</script>
+
+<style scoped>
+.cardGridItem {
+  @apply p-6;
+  @apply relative;
+  @apply aspect-square border;
+  /* @apply basis-1/3 flex-grow-0 ; */
+}
+
+.cardGridItem .cta {
+  @apply absolute bottom-4;
+}
+
+.content {
+  /* @apply flex flex-wrap flex-row content-between; */
+}
+</style>
---
+++ storyblok/cardGrid.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="container">
+  <div class="cardGrid grid sm:grid-cols-2 md:grid-cols-4 gap-6">
+    <div class="cardGridItem"></div>
+    <div class="cardGridItem"></div>
+    <cardGridItem
+      v-for="blok in blok.items"
+      :key="blok._uid"
+      :blok="blok"
+    />
+  </div>
+</section>
+</template>
+ 
+<script setup>
+defineProps({ blok: Object })
+</script>
+
+<style>
+.cardGridItem {
+  @apply md:relative;
+  @apply md:aspect-square;
+}
+</style>
