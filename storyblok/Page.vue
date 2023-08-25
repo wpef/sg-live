33e4ee99619cc7789ef752a4582ce8788f220f86
fdemoncade <f.demoncade@gmail.com>
2023-08-25 13:46:37 +0200

    DONE : Hero

--- storyblok/Page.vue
+++ storyblok/Page.vue
@@ -1,6 +1,19 @@
 <template>
   <div v-editable="blok" class="page">
-    <StoryblokComponent v-for="blok in blok.body" :key="blok._uid" :blok="blok" />
+    <div class="hero" :class="blok.heroImage?.filename ? 'h-screen' : ''" :style="{
+      backgroundColor: blok.heroBgColor !== '' ? blok.heroBgColor : '#003966',
+      backgroundImage: `url(${blok.heroImage?.filename})`,
+    }">
+      <div class="container">
+        <div class="heroTitle text-white">
+          <h1 class="h1" v-if="blok.title">{{ blok.title }}</h1>
+          <h2 class="h2" v-if="blok.subtitle">{{ blok.subtitle }}</h2>
+        </div>
+      </div>
+    </div>
+    <div class="body">
+    <StoryblokComponent class="body" v-for="blok in blok.body" :key="blok._uid" :blok="blok" />
+    </div>
   </div>
 </template>
  
@@ -9,11 +22,33 @@ defineProps({ blok: Object })
 </script>
 
 <style>
-.p1.text-white, .p2.text-white {
-  color : white;
+/*local*/
+div.hero {
+  @apply pt-64;
+  @apply relative bg-cover;
+  min-height: 30rem;
+  margin-top: 0 !important;
 }
 
-.h1, .text h1 {
+.hero .heroTitle {
+  @apply pr-96;
+  @apply absolute bottom-12;
+  max-width: 1440px; 
+}
+
+.hero .heroTitle h1 {
+  @apply pb-6;
+}
+
+
+/*global*/
+.p1.text-white,
+.p2.text-white {
+  color: white;
+}
+
+.h1,
+.text h1 {
   /* SG/Head 1 */
   font-family: Inter;
   font-size: 64px;
@@ -24,7 +59,8 @@ defineProps({ blok: Object })
   letter-spacing: -3.84px;
 }
 
-.h2, .text h2 {
+.h2,
+.text h2 {
   /* SG/Head 2 */
   font-family: Inter;
   font-size: 40px;
@@ -35,7 +71,8 @@ defineProps({ blok: Object })
   letter-spacing: -2px;
 }
 
-.h3, .text h3 {
+.h3,
+.text h3 {
   /* SG/Head 3 */
   font-family: Inter;
   font-size: 26px;
@@ -46,7 +83,8 @@ defineProps({ blok: Object })
   letter-spacing: -1.04px;
 }
 
-.h4, .text h4 {
+.h4,
+.text h4 {
   /* SG/Head 4 */
   font-family: Inter;
   font-size: 20px;
@@ -57,7 +95,8 @@ defineProps({ blok: Object })
   letter-spacing: -0.8px;
 }
 
-.h5, .text h5 {
+.h5,
+.text h5 {
   /* SG/Head 5 */
   font-family: Inter;
   font-size: 20px;
@@ -68,7 +107,8 @@ defineProps({ blok: Object })
   letter-spacing: -0.4px;
 }
 
-.h6, .text h6 {
+.h6,
+.text h6 {
   /* SG/Head 6 */
   font-family: Inter;
   font-size: 16px;
@@ -155,21 +195,30 @@ li {
   letter-spacing: -0.28px;
 }
 
-section {
+.body section {
   @apply mt-4 mb-6 pb-6 pt-4;
 }
 
 .container {
-  @apply mt-4 mb-6 pb-6 pt-4 px-16; /* à voir*/
+  @apply mt-4 mb-6 pb-6 pt-4 px-16;
+  /* à voir*/
   max-width: 1440px;
   margin: auto;
 }
 
-h1, h2, h3, h4, h5, h6,
-p, li, button, span, a {
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p,
+li,
+button,
+span,
+a {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   font-smooth: never;
 }
-
 </style>
