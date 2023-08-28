68336face6a49c9bf7171afbdc8c8ecdcc7036d3
fdemoncade <f.demoncade@gmail.com>
2023-08-28 17:51:40 +0200

    DONE : separator

--- storyblok/separator.vue
+++ storyblok/separator.vue
@@ -1,10 +1,14 @@
 <template>
   <section class="separator">
     <div class="container">
-      <div class="border-t"></div>
+      <div :class="blok.line ? 'border-t' : ''"></div>
   </div>
   </section>
 </template>
+
+<script setup>
+const props = defineProps({ blok: Object })
+</script>
 
 <style scoped>
 
