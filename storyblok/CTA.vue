ace22f9e90b6d110e9bc60aef21babe7ec029f51
fdemoncade <f.demoncade@gmail.com>
2023-08-30 22:15:08 +0200

    REFACTO : link resolve + icon

--- storyblok/CTA.vue
+++ storyblok/CTA.vue
@@ -1,16 +1,35 @@
 <template>
-    <NuxtLink class="cta flex items-center"
-    :to="blok.link.url == '' ? '/' + blok.link.cached_url : blok.link.url"
+  <!-- {{ blok.link }} -->
+  <NuxtLink class="cta flex items-center"
+    :class="blok.white ? 'text-white' : 'text-[#3052B7]'"
+    :to="toUrl(blok)"
     >
-      <span :class="blok.white ? 'text-white' : 'text-[#3052B7]'">{{ blok.label }}</span>
-      <img class="ml-2" :src="iconSrc">
+      <span>{{ blok.label }}</span>
+      <img v-if="blok.icon !== false" class="ml-2" :src="iconSrc">
     </NuxtLink>
 </template>
  
 <script setup>
 const props = defineProps({ 
   blok: Object,
-  white: Boolean
+  white: Boolean,
+  icon : Boolean
 });
 const iconSrc = computed(() => { return props.blok.white || props.white ? '/assets/icons/next-white.svg' : '/assets/icons/next.svg'})
+const toUrl = function (blok) {
+  const link = blok.link;
+  const https = "https://";
+
+
+  // console.log('link.url.startsWith(https)', link.url.startsWith(https))
+
+  if (link.linktype === 'story') {
+    return '/' + link.cached_url
+  }
+  if (link.linktype == 'url') {
+    if (link.url.startsWith(https))
+      return link.url
+    return https + link.url
+  }
+}
 </script>
