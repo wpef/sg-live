e81bc77d64dabde71abe00558442117fbe82211d
fdemoncade <f.demoncade@gmail.com>
2023-09-11 21:23:50 +0200

    NEW : handled text Separator

---
+++ storyblok/textSeparator.vue
@@ -0,0 +1,9 @@
+<template>
+  <div :class="blok.line ? 'border-t ' : '' + space"></div>
+</template>
+<script setup>
+const props = defineProps({ blok: Object })
+const space = computed(() => {
+  return "pt-"+props.blok.space;
+})
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   content: ['storyblok/**/*.{vue,js}', 'components/**/*.{vue,js}', 'pages/**/*.vue'],
+  // To pre-import classes (computed)
+  safelist: [
+    'pt-0', 'pt-1','pt-4','pt-12', 'pt-28', 'pt-48', 'pt-96',
+  ]
 }
