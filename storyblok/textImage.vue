badf3ee3078ba0ee9681e46c6cad5f9367f64373
fdemoncade <f.demoncade@gmail.com>
2023-09-12 21:11:07 +0200

    FIX : textImage

--- storyblok/textImage.vue
+++ storyblok/textImage.vue
@@ -1,11 +1,11 @@
 <template>
   <section class="textImage" :style="{ backgroundColor : blok.backgroundColor }">
     <div class="container">
-    <div class="title md:w-1/2 md:pr-48">
-      <h4 v-if="blok.title" :class="blok.white ? 'text-white' : ''" class="h2">{{ blok.title }}</h4>
+    <div class="title">
+      <h2 v-if="blok.title" :class="blok.white ? 'text-white' : ''" class="h2">{{ blok.title }}</h2>
       <div v-if="blok.body" :class="blok.white ? 'text-white' : ''" class="text" v-html="content"></div>
     </div>
-    <div class="thumb md:w-1/2">
+    <div class="thumb w-full lg:w-1/2">
       <NuxtImg
         v-if="blok.image"
         provider="storyblok"
@@ -28,11 +28,25 @@ console.log(content)
 <style scoped>
 .container {
   @apply flex flex-wrap flex-row;
-  @apply py-12 my-2;
+  @apply py-6 lg:py-12;
 }
 
 .title {
   @apply flex flex-wrap flex-row content-between;
+  @apply lg:w-1/2;
+  @apply pb-14 md:pb-0 md:pr-12 xl:pr-40;
+}
+
+.title h2 {
+  @apply pb-5;
+}
+
+.title .text {
+  @apply lg:pr-20;
+}
+
+.text .p {
+  @apply pb-0;
 }
 
 
