e2e0ff8b6f1c59a8b94da42568f05defadbab5ad
fdemoncade <f.demoncade@gmail.com>
2023-09-30 18:46:40 +0200

    ADD : hover animation

--- components/Header.vue
+++ components/Header.vue
@@ -58,9 +58,10 @@ const handleMouseOut = () => {
           </li>
         </ul>
       </nav>
-      <NuxtLink class="hidden md:flex items-center text-white bg-[#3052B7] px-5 py-3 rounded" :to="headerCTA[0].link.url" target="_blank">
+      <NuxtLink class="hidden md:flex items-center gap-2 text-white bg-[#3052B7] px-5 py-3 rounded cta" :to="headerCTA[0].link.url" target="_blank">
+        <img class="icon-left" src="/icons/up-white.svg">
         <span>{{ headerCTA[0].label }}</span>
-        <img class="ml-2" src="/icons/up-white.svg">
+        <img class="icon-right" src="/icons/up-white.svg">
       </NuxtLink>
     </div>
   </header>
@@ -91,6 +92,38 @@ a {
   font-weight: 500;
   line-height: 100%;
   letter-spacing: -0.02rem;
+}
+
+.cta {
+  position: relative;
+}
+
+.cta .icon-left {
+  @apply -translate-x-2 absolute opacity-0 duration-300;
+}
+
+.cta .icon-right {
+  @apply translate-x-0  duration-300;
+}
+
+.cta span {
+  @apply translate-x-0 duration-300;
+}
+
+.cta:hover span {
+  @apply translate-x-8;
+}
+
+.cta:hover .icon-right {
+  @apply -translate-x-2 opacity-0;
+}
+
+.cta:hover .icon-left {
+  @apply translate-x-0 opacity-100;
+}
+
+.cta .icon-left, .cta a:hover .icon-right{
+  /* @apply -translate-x-10; */
 }
 
 .logo {
