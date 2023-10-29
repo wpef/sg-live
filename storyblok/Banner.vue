1ba8288e695784dca65c14fe05dc660a25bcf9ba
fdemoncade <f.demoncade@gmail.com>
2023-10-30 00:31:37 +0100

    ADDED : Banner

---
+++ storyblok/Banner.vue
@@ -0,0 +1,39 @@
+<template>
+<div v-editable="blok" class="banner" :style="{
+      backgroundColor: blok.bgColor !== '' ? blok.bgColor : '#003966',
+      backgroundImage: `url(${blok.bgImage.filename})`,
+    }">
+      <div class="container">
+        <h2 class="h3 text-white" 
+            v-if="blok.title" 
+            data-aos="fade-up"
+          >{{ blok.title }}</h2>
+          <CTA v-if="blok.cta[0]"
+            :blok="blok.cta[0]"
+            data-aos="fade-up"
+          />
+        </div>
+    </div>
+</template>
+ 
+<script setup>
+defineProps({ blok: Object })
+</script>
+
+<style scoped>
+.banner {
+  @apply h-96;
+  @apply relative bg-cover bg-center;
+  margin-top: 0 !important;
+}
+
+.container {
+  @apply pr-12 lg:pr-48 xl:pr-[42rem];
+  @apply absolute bottom-12;
+  max-width: 1440px;
+}
+
+h2 {
+  @apply mb-5;
+}
+</style>
