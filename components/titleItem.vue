b7616b4e7db66d406e6846b9bf45776ddc58c2e4
fdemoncade <f.demoncade@gmail.com>
2023-11-14 13:59:04 +0100

    ADD : Contact

---
+++ components/titleItem.vue
@@ -0,0 +1,26 @@
+<template>
+    <h2 class="title h4" data-aos="fade-up">{{ blok.title }}</h2>
+    <p class="transition-all content" data-aos="fade-up" v-for="line in content">{{ line }}</p>
+</template>
+
+<style scoped>
+.h4 {
+  @apply pt-8 pb-5;
+  color: var(--sg-color-styles-noir-texte, #1E1E1E);
+
+  /* SG Text styles/Head 4 */
+  font-family: Inter;
+  font-size: 1.25rem;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 132%; /* 1.65rem */
+  letter-spacing: -0.05rem;
+}
+</style>
+ 
+<script setup>
+const props = defineProps({
+  blok: Object,
+})
+const content = computed(() => props.blok.content.split('\n'));
+</script>
---
+++ storyblok/ContactPage.vue
@@ -0,0 +1,68 @@
+<template>
+
+<PageHero v-if="blok.title || blok.subtitle" :blok="blok"/>
+<div class="body">
+  <section class="textImage" :style="{ backgroundColor : blok.backgroundColor }">
+    <div class="container" v-editable="blok">
+    <div class="text">
+      <div class="info-wrapper">
+
+        <div class="infos w-1/2"  v-for="blok in blok.infos" :key="blok._uid" :blok="blok">
+          <TitleItem :blok="blok"/>
+        </div>
+      
+      </div>
+      <div class="booking-wrapper flex flex-col lg:w-1/2">
+        <div class="content">
+          <TitleItem class="content" :blok="blok.booking[0]"/>
+        </div>
+        <CTA :blok="blok.booking[1]"/>
+      </div>
+    </div>
+    <div class="image w-full lg:w-1/2"  data-aos="fade-up" data-aos-delay="500">
+      <NuxtImg
+        v-if="blok.image"
+        provider="storyblok"
+        :src="blok.image?.filename" 
+        :alt="blok.image?.alt"
+        :modifiers="{ filters: { focal: blok.image?.focus } }"
+        height="680"
+        width="648"
+      />
+    </div>
+  </div>
+  </section>
+</div>
+</template>
+
+<script setup>
+const props = defineProps({ blok: Object });
+const content = computed(() => renderRichText(props.blok.body));
+</script>
+
+<style scoped>
+.container {
+  @apply flex flex-wrap flex-row;
+  @apply py-6 lg:py-12;
+}
+
+.text {
+  @apply flex flex-wrap flex-row content-between;
+  @apply lg:w-1/2;
+  @apply pb-14 md:pb-0;
+}
+
+.text .p {
+  @apply pb-0;
+}
+
+.info-wrapper {
+  @apply w-full;
+  @apply flex flex-row gap-4;
+}
+
+.booking-wrapper .content {
+  @apply  py-8 lg:pt-16
+}
+
+</style>
