e14957c393b868d79da384bca6a62697b5387d0e
fdemoncade <f.demoncade@gmail.com>
2023-11-20 21:05:40 +0100

    fix : list numbered

--- storyblok/textList.vue
+++ storyblok/textList.vue
@@ -1,24 +1,31 @@
 <template>
     <ul v-editable="blok" class="textList mb-4" :class="blok.columns == 2 ? 'columns-2' : ''">
-      <li v-for="item in content">{{ item }}</li>
+      <li :class="blok.number ? 'number' : 'icon'" v-for="item in content">{{ item }}</li>
     </ul>
 </template>
 
 <style scoped>
+
 li {
   color: var(--sg-gris-texte, #505050);
   font-size: 16px;
   margin-bottom: .88rem;
   line-height: 1.4rem;
   letter-spacing: -0.32px;
+  @apply pl-8 mr-12;
+  
+}
+li.icon {
   background-repeat: no-repeat;
   background-image: url("/icons/check.svg");
-  
-  @apply pl-8 mr-12;
+}
+
+li.number {
+  list-style-type: decimal;
 }
 </style>
  
 <script setup>
 const props = defineProps({ blok: Object })
-const content = computed(() => props.blok.list.split("\n"));
+const content = computed(() => props.blok.list.split("\n").filter( n => n));
 </script>
