80f52535923319b1321030d5c87e4c1c6c290293
fdemoncade <f.demoncade@gmail.com>
2023-11-26 17:09:09 +0100

    DP_FIX : textSeparator réduire l’quivalent d’espace sur mobile

--- storyblok/textSeparator.vue
+++ storyblok/textSeparator.vue
@@ -4,6 +4,6 @@
 <script setup>
 const props = defineProps({ blok: Object })
 const space = computed(() => {
-  return "pt-"+props.blok.space;
+  return "pt-"+props.blok.space/2 + " md:pt-"+props.blok.space;
 })
 </script>
