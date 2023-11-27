2767b529955e8d49842d9d992de17ff472067843
fdemoncade <f.demoncade@gmail.com>
2023-11-27 12:33:22 +0100

    FIX : display partners

--- components/PartnerBanner.vue
+++ components/PartnerBanner.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="PartnerBanner wrapper flex flex-row gap-8">
+  <div class="PartnerBanner wrapper flex flex-row gap-8" v-if="logos && logos[0]">
     <div class="item flex items-center w-28" v-for="item in logos">
       <img :src="item.filename" :alt="item.alt"/>
     </div>
